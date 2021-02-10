1b758bd1222bb227cd51a1b1aa3dde5f522eadb2
MadhumithaMadhavan <78358548+MadhumithaMadhavan@users.noreply.github.com>
2021-02-10 17:22:56 +0530

    Add files via upload

---
+++ coordinate.py
@@ -0,0 +1,60 @@
+import csv
+import random
+def loadDataset(filename, split, trainingSet=[] , testSet=[]):
+	with open(filename, 'r') as csvfile:
+	    lines = csv.reader(csvfile)
+	    dataset = list(lines)
+	    for x in range(1,len(dataset)-1):
+	        for y in range(2):
+	            dataset[x][y] = float(dataset[x][y])
+	        if random.random() < split:
+	            trainingSet.append(dataset[x])
+	        else:
+	            testSet.append(dataset[x])
+
+import math
+def euclideanDistance(instance1, instance2, length):
+	distance = 0
+	for x in range(length):
+		distance += pow((instance1[x] - instance2[x]), 2)
+	return math.sqrt(distance)
+import operator 
+def getNeighbors(trainingSet, testInstance, k):
+	distances = []
+	length = len(testInstance)-1
+	for x in range(len(trainingSet)):
+		dist = euclideanDistance(testInstance, trainingSet[x], length)
+		distances.append((trainingSet[x], dist))
+	distances.sort(key=operator.itemgetter(1))
+	neighbors = []
+	for x in range(k):
+		neighbors.append(distances[x][0])
+	return neighbors
+import operator
+def getResponse(neighbors):
+	classVotes = {}
+	for x in range(len(neighbors)):
+		response = neighbors[x][-1]
+		if response in classVotes:
+			classVotes[response] += 1
+		else:
+			classVotes[response] = 1
+	sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+	# print(sortedVotes[0][0])
+	return sortedVotes[0][0]
+trainingSet=[]
+testSet=[]
+loadDataset('E:/vb/Book2.csv', 0.66, trainingSet, testSet)
+print ('Train: ' + repr(len(trainingSet)))
+print ('Test: ' + repr(len(testSet)))
+predictions=[]
+# print(predictions)
+k = 3
+for x in range(len(testSet)):
+		neighbors = getNeighbors(trainingSet, testSet[x], k)
+		result = getResponse(neighbors)
+		predictions.append(result)
+		# print(predictions)
+		# print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
+print ('The predicted coordinate is ' +repr(result))
+
